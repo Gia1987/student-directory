9dd1485a424d5d5dd087437bfa2811b7847c50cf
Giacomo Ninniri <giacomo.n27@gmail.com>
2018-02-16 14:42:08 +0000

    Refactory the code

--- directory.rb
+++ directory.rb
@@ -1,39 +1,9 @@
-=begin
-# let's put all students into an array
-students = [ 
-  {name:"Dr. Hannibal Lecter", cohort: :movember},
-  {name:"Darth Vader", cohort: :movember},
-  {name:"Nurse Ratched", cohort: :movember},
-  {name:"Micheal Corleone", cohort: :movember},
-  {name:"Alex DeLarge", cohort: :movember},
-  {name:"The Wiched Whitch of the West", cohort: :movember},
-  {name:"Terminator", cohort: :movember},
-  {name:"Freddy Krueger", cohort: :movember},
-  {name:"The Joker", cohort: :movember},
-  {name:"Joffrey Baratheon", cohort: :movember},
-  {name:"Norman Betes", cohort: :movember}
-  ]
-=end
-=begin
-students = [
-    "Dr. Hannibal Lecter",
-    "Darth Vader",
-    "Nurse Ratched",
-    "Michael Corleone",
-    "Alex DeLarge",
-    "The Wicked Witch of the West",
-    "Terminator",
-    "Freddy Krueger",
-    "The Joker",
-    "Joffrey Baratheon",
-    "Norman Bates"
-]
-=end
+@students = [] # an empty array accessible to all methods
+
 def input_students
   puts "Please enter the names of the students"
   puts "to finish, just hit return twice"
   #create an empty array
-  students = []
   #get the first name
   name = gets.chop
   if name.empty?
@@ -53,40 +23,73 @@ def input_students
     country = gets.chop
     puts "Add height (cm)"
     height = gets.chop
-    students <<  {name: name, cohort: month, hobbies: hobby, origin: country, height: height}
-    puts "Now we have #{students.count} student"
+    @students <<  {name: name, cohort: month, hobbies: hobby, origin: country, height: height}
+    puts "Now we have #{@students.count} " + (@students.count > 1 ? 'students' : 'student')
     #get another name from the user
     name = gets.chop
   end
-  #return the array of students
-  students
 end
 
-def interactive_menu
-  students = []
-  loop do 
-    #1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    #2. read the input and save it into a variable
-    selection = gets.chop
-    #3. do what the user has asked
-    case selection
+def print_students_list
+  if !@students.empty?
+    ask_for_input
+    input = gets.chop
+    i = 0
+    while i < @students.count
+      # Students grouped by cohort
+      if input == "cohort"
+        grouped_by_cohorts
+        break
+      end
+      # print names that starts with a particular letter
+      if "#{@students[i][:name]}".start_with?(input.upcase) && "#{@students[i][:name]}".length < 12
+        puts "#{(i + 1).to_s}. #{@students[i][:name]} (#{@students[i][:cohort]} cohort), who loves #{@students[i][:hobbies]}, comes from #{@students[i][:origin]} and also is  #{@students[i][:height]} cm tall.".center(120,)
+      elsif input == "All"
+        puts "#{(i + 1).to_s}. #{@students[i][:name]} (#{@students[i][:cohort]} cohort), who loves #{@students[i][:hobbies]}, comes from #{@students[i][:origin]} and also is  #{@students[i][:height]} cm tall.".center(120,)
+      end
+      i += 1
+    end
+  end
+end
+
+def grouped_by_cohorts
+  student_by_cohort = {}
+  @students.each do |student|
+    cohort = student[:cohort]
+    if student_by_cohort[cohort] == nil
+       student_by_cohort[cohort] = []
+    end
+    student_by_cohort[cohort].push(student[:name])
+  end
+  student_by_cohort.each {|key, value| puts "#{key} : " + value.join(", ")}
+end
+
+def ask_for_input
+  puts 'Type "All" to see all the students or a letter A..Z to see the names that starts with:'  
+  puts "Type 'cohort' to see the student grouped by own cohort:"
+end
+def proccess(selection)
+  case selection
     when "1"
-      #input the students
-      students = input_students
+    #input the students
+      input_students
     when "2"
-      #show the students
-      print_header
-      print(students)
-      print_footer(students)
+    #show the students
+      show_students
     when "9"
       exit #this will couse the program to terminate
     else
       puts " I don't know you meant, try again"
-    end
   end
+end
+
+def interactive_menu
+  loop do 
+    print_menu
+    #2. read the input and save it into a variable
+    proccess(gets.chop)
+    #3. do what the user has asked
+   end
 end
 
 def print_header
@@ -94,49 +97,27 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  if !students.empty?
-    puts 'Type "All" to see all the students or a letter A..Z to see the names that starts with:'  
-    puts "Type 'cohort' to see the student grouped by own cohort:"
-    input = gets.chop
-    i = 0
-    while i < students.count
-      # Students grouped by cohort
-      if input == "cohort"
-        student_by_cohort = {}
-        students.each do |student|
-          cohort = student[:cohort]
-          if student_by_cohort[cohort] == nil
-             student_by_cohort[cohort] = []
-          end
-          student_by_cohort[cohort].push(student[:name])
-        end
-        student_by_cohort.each {|key, value| puts "#{key} : " + value.join(", ")}
-        break
-      end
-      # print names that starts with a particular letter
-      if "#{students[i][:name]}".start_with?(input.upcase) && "#{students[i][:name]}".length < 12
-        puts "#{(i + 1).to_s}.  #{students[i][:name]} (#{students[i][:cohort]} cohort), who loves #{students[i][:hobbies]}, comes from #{students[i][:origin]} and also is  #{students[i][:height]} cm tall.".center(150,"-")
-      elsif input == "All"
-        puts "#{(i + 1).to_s}.  #{students[i][:name]} (#{students[i][:cohort]} cohort), who loves #{students[i][:hobbies]}, comes from #{students[i][:origin]} and also is  #{students[i][:height]} cm tall.".center(150,"-")
-      end
-      i += 1
-    end
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} student"
+  else
+    puts "Overall, we have #{@students.count} great students" 
   end
 end
 
-def print_footer(names)
-  if names.count == 1
-    puts "Overall, we have #{names.count} student"
-  else
-    puts "Overall, we have #{names.count} great students" 
-  end
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items  
 end
 
 interactive_menu
-students = input_students
-# nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
+@students = input_students
+
 
