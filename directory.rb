8d9278499d8995d30d28a902af098f3fff0c6ff0
Giacomo Ninniri <giacomo.n27@gmail.com>
2018-02-17 10:47:44 +0000

    tidy up the code

--- directory.rb
+++ directory.rb
@@ -1,93 +1,20 @@
 @students = [] # an empty array accessible to all methods
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "to finish, just hit return twice"
-  #create an empty array
-  #get the first name
-  name = STDIN.gets.chomp
-  if name.empty?
-    puts "There is not any student on the list!"
-  end
-  # while the name is not empty, repeat this code
-  while !name.empty? do 
-    # add the student hash to the array
-    puts "What cohort are you part of ?"
-    month = gets.chomp
-    if month == ""
-      month = "february" 
-    end
-    puts "Add the hobbies"
-    hobby = gets.chomp
-    puts "Add country"
-    country = gets.chomp
-    puts "Add height (cm)"
-    height = gets.chomp
-    @students <<  {name: name, cohort: month, hobbies: hobby, origin: country, height: height}
-    puts "Now we have #{@students.count} " + (@students.count > 1 ? 'students' : 'student')
-    #get another name from the user
-    name = STDIN.gets.chomp
-  end
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the lidt to students.csv"
+  puts "4. Load list from students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items  
 end
 
-def save_students
-  #open the file for writing
-  file = File.open("students.csv", "w")
-  #iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
-def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end
-
-def print_students_list
-  if !@students.empty?
-    ask_for_input
-    input = gets.chomp
-    i = 0
-    while i < @students.count
-      # Students grouped by cohort
-      if input == "cohort"
-        grouped_by_cohorts
-        break
-      end
-      # print names that starts with a particular letter
-      if "#{@students[i][:name]}".start_with?(input.upcase) && "#{@students[i][:name]}".length < 12
-        puts "#{(i + 1).to_s}. #{@students[i][:name]} (#{@students[i][:cohort]} cohort), who loves #{@students[i][:hobbies]}, comes from #{@students[i][:origin]} and also is  #{@students[i][:height]} cm tall.".center(120,)
-      elsif input == "All"
-        puts "#{(i + 1).to_s}. #{@students[i][:name]} (#{@students[i][:cohort]} cohort), who loves #{@students[i][:hobbies]}, comes from #{@students[i][:origin]} and also is  #{@students[i][:height]} cm tall.".center(120,)
-      end
-      i += 1
-    end
-  end
-end
-
-def grouped_by_cohorts
-  student_by_cohort = {}
-  @students.each do |student|
-    cohort = student[:cohort]
-    if student_by_cohort[cohort] == nil
-      student_by_cohort[cohort] = []
-    end
-    student_by_cohort[cohort].push(student[:name])
-  end
-  student_by_cohort.each {|key, value| puts "#{key} : " + value.join(", ")}
-end
-
-def ask_for_input
-  puts 'Type "All" to see all the students or a letter A..Z to see the names that starts with:'  
-  puts "Type 'cohort' to see the student grouped by own cohort:"
+def interactive_menu
+  loop do 
+    print_menu
+    #2. read the input and save it into a variable
+    proccess(STDIN.gets.chomp)
+    #3. do what the user has asked
+   end
 end
 
 def proccess(selection)
@@ -109,13 +36,37 @@ def proccess(selection)
   end
 end
 
-def interactive_menu
-  loop do 
-    print_menu
-    #2. read the input and save it into a variable
-    proccess(STDIN.gets.chomp)
-    #3. do what the user has asked
-   end
+def input_students
+  puts "Please enter the names of the students"
+  puts "to finish, just hit return twice"
+  #create an empty array
+  #get the first name
+  name = STDIN.gets.chomp
+  if name.empty?
+    puts "There is not any student on the list!"
+  end
+  # while the name is not empty, repeat this code
+  while !name.empty? do 
+    # add the student hash to the array
+    puts "What cohort are you part of ?"
+    cohort = gets.chomp
+    if cohort == ""
+      cohort = "february" 
+    end
+    storage_students(name, cohort)
+    puts "Now we have #{@students.count} " + (@students.count > 1 ? 'students' : 'student')
+    #get another name from the user
+    name = STDIN.gets.chomp
+  end
+end
+
+def storage_students(name, cohort)
+  @students <<  {name: name, cohort: cohort.to_sym }
+end
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def print_header
@@ -123,26 +74,69 @@ def print_header
   puts "-------------"
 end
 
-def print_footer
-  if @students.count == 1
-    puts "Overall, we have #{@students.count} student"
-  else
-    puts "Overall, we have #{@students.count} great students" 
+def ask_for_input
+  puts 'Type "All" to see all the students or a letter A..Z to see the names that starts with:'  
+  puts "Type 'cohort' to see the student grouped by own cohort:"
+end
+
+def print_students_list
+  if !@students.empty?
+    ask_for_input
+    input = gets.chomp
+    i = 0
+    while i < @students.count
+      # Students grouped by cohort
+      if input == "cohort"
+        grouped_by_cohorts
+        break
+      end
+      # print names that starts with a particular letter
+      if "#{@students[i][:name]}".start_with?(input.upcase)
+        puts "#{(i + 1).to_s}. #{@students[i][:name]} (#{@students[i][:cohort]} cohort)".center(120,)
+      elsif input == "All"
+        puts "#{(i + 1).to_s}. #{@students[i][:name]} (#{@students[i][:cohort]} cohort)".center(120,)
+      end
+      i += 1
+    end
   end
 end
 
-def show_students
-  print_header
-  print_students_list
-  print_footer
+def grouped_by_cohorts
+  student_by_cohort = {}
+  @students.each do |student|
+    cohort = student[:cohort]
+    if student_by_cohort[cohort] == nil
+      student_by_cohort[cohort] = []
+    end
+    student_by_cohort[cohort].push(student[:name])
+  end
+  student_by_cohort.each {|key, value| puts "#{key} : " + value.join(", ")}
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the lidt to students.csv"
-  puts "4. Load list from students.csv"
-  puts "9. Exit" # 9 because we'll be adding more items  
+
+def print_footer
+  puts "Overall, we have #{@students.count} " + (@students.count > 1 ? 'students' : 'student')
+end
+
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+   storage_students(name, cohort)
+  end
+  file.close
 end
 
 def try_load_students
